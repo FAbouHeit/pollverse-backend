ada591cbdf82eebda3476255eff19a3a3ee67648
Fuad Abou Heit <f.abouheit@gmail.com>
2024-02-29 17:43:00 +0200

    fix 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connect from './Config/Mongo.Config.js';
+import dotenv from "dotenv";
+import cors from "cors";
+import connect from "./Config/Mongo.Config.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -14,38 +14,39 @@ import transactionRouter from "./Routes/Transaction.Routes.js";
 import userRouter from "./Routes/User.Routes.js";
 import profanityRouter from "./Routes/Profanity.Router.js";
 
-
-
 const PORT = process.env.PORT || 6666;
 const app = express();
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const corsOption = {
-    origin: process.env.FRONT_END_PATH,
-    credentials: true,
-    optionsSuccessStatus: 200,
-}
+// const corsOption = {
+//   origin: process.env.FRONT_END_PATH,
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 
 app.use(cookieParser());
-app.use(cors(corsOption));
+app.use(cors());
 
-app.use('/activity', activityRouter);
-app.use('/chat', chatRouter);
-app.use('/comment', commentRouter);
-app.use('/notification', notificationRouter);
-app.use('/post', postRouter);
-app.use('/transaction', transactionRouter);
-app.use('/user', userRouter);
-app.use('/profanity', profanityRouter);
+app.use("/activity", activityRouter);
+app.use("/chat", chatRouter);
+app.use("/comment", commentRouter);
+app.use("/notification", notificationRouter);
+app.use("/post", postRouter);
+app.use("/transaction", transactionRouter);
+app.use("/user", userRouter);
+app.use("/profanity", profanityRouter);
 
-app.listen(PORT, ()=>{
-    connect();
-    console.log(`running on port: ${PORT}`);
-    if(PORT === 6666){
-        console.log("ERROR: issue reading port from process.env. Continue with caution! ...");
-    }
-})
+app.listen(PORT, () => {
+  connect();
+  console.log(`running on port: ${PORT}`);
+  if (PORT === 6666) {
+    console.log(
+      "ERROR: issue reading port from process.env. Continue with caution! ..."
+    );
+  }
+});
 
 //add socket io
 //add authentication, authorization
