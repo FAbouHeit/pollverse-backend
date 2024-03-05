2359aa7dbf144ef990fd56d8b89f08dd909d8e6b
Fuad Abou Heit <f.abouheit@gmail.com>
2024-03-05 14:58:29 +0200

    testing something

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // const corsOption = {
 //   origin: '*',
