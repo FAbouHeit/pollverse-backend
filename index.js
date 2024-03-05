f2a44cad62a0a5abcacbef71541603744c943f65
Fuad Abou Heit <f.abouheit@gmail.com>
2024-03-05 11:24:31 +0200

    production4

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const corsOption = {
-  origin: process.env.FRONT_END_PATH,
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200,
 };
