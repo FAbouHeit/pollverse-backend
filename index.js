0a700bfe3b862ffc62961f3e2de9a5de84fd4071
Fuad Abou Heit <f.abouheit@gmail.com>
2024-03-05 14:56:16 +0200

    new test

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ app.use(express.static("public"));
 //   optionsSuccessStatus: 200,
 // };
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE,PATCH"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 app.use(cookieParser());
 
 app.use("/activity", activityRouter);
